d2a356477d94a19ece4a64f8264e3e2a685defda
Gilbert2894 <gilbert0894@hotmail.com>
2016-10-30 18:37:24 -0600

    COnectado con mongo

--- data/dtusers.php
+++ data/dtusers.php
@@ -6,65 +6,54 @@ class dtusers{
 	}
 
 	function insertar($user){
-		$con = new dtConexion;
-		if($con->conectar()==true){
-
-			$query = "CALL sp_insertarUsers('".$user->getusername()."',
-						'".$user->getpassword()."',
-						'".$user->getfname()."',
-						'".$user->getlname()."', 
-						'".$user->getphone()."',
-						'".$user->getcellphone()."',
-						'".$user->getemail()."')";
-
-
-			$result = @mysql_query($query);
-			//echo "$query";
-			if (!$result){
-				return false;
-			}else{
-				return true;
-			}
+		$con = new MongoClient();
+		$db=$con->registroPracticantes;
+		$collection=$db->usuarios;
+		$obj=array("usuario" => $user->getusername(),"contrasenia" =>$user->getpassword(),"nombre"=>$user->getfname(),"apellido"=>$user->getlname(),"telefono"=>$user->getphone(),"celular"=>$user->getcellphone(),"correo"=>$user->getemail());
+		try {
+			$collection->insert($obj);
+			return true;
+		} catch (Exception $e) {
+			return false;
 		}
 	}
 
 	function consultas(){
-		$con = new dtConexion;
-		// se declara una lista enlazada
+		$con = new MongoClient();
+		$db=$con->registroPracticantes;
+		$collection=$db->usuarios;
 		$lista = array();
-
-		if($con->conectar()==true){
-
-			$query = "CALL sp_consultasUsers()";
-			$result = @mysql_query($query);
-			//echo "$query";
-			while($row = mysql_fetch_array($result)){
-
-	 			$micompany = new users( $row[0], $row[1] , $row[3], $row[4], $row[5], $row[6], $row[7]);	
-				array_push($lista, $micompany);
-			}
-			if (!$lista){
-				return false;
-			}else{
-				return $lista;
-			}
+		$cursor=$collection->find();
+		foreach ($cursor as $value) {
+			$user = new users;
+			$user->setid($value['_id']);
+			$user->setusername($value['usuario']);
+			$user->setpassword($value['contrasenia']);
+			$user->setfname($value['nombre']);
+			$user->setlname($value['apellido']);
+			$user->setphone($value['telefono']);
+			$user->setcellphone($value['celular']);
+			$user->setemail($value['correo']);
+			array_push($lista, $user);
 		}
+		return $lista;
 	}
 
 
 	function eliminar($id){
-		$con = new dtConexion;
-		if($con->conectar()==true){
-
-			$query = "CALL sp_eliminarUsers($id)";
-			$result = @mysql_query($query);
-
-			if (!$result){
-				return false;
-			}else{
-				return $result;
-			}
+		$con = new MongoClient();
+		$db=$con->registroPracticantes;
+		$collection=$db->usuarios;
+		$obj=array("_id" => new MongoId($id));
+		try {
+			$collection->remove($obj);
+			return true;
+			
+		} catch (Exception $e) {
+			
+			return false;
 		}
+
 	}
 
 	function consulta($id){
@@ -90,23 +79,15 @@ class dtusers{
 	}
 
 	function modificar($micompany){
-		$con = new dtConexion;
-		if($con->conectar()==true){
-			$query = "CALL sp_modificarUsers(".$micompany->getid().",
-						'".$micompany->getusername()."',
-						'".$micompany->getpassword()."',
-						'".$micompany->getfname()."',
-						'".$micompany->getlname()."',
-						'".$micompany->getphone()."',
-						'".$micompany->getcellphone()."',
-							'".$micompany->getemail()."')";
-				//echo "$query";
-			$result = @mysql_query($query);
-			if (!$micompany){
-				return false;
-			}else{
-				return true;
-			}
+		$con = new MongoClient();
+		$db=$con->registroPracticantes;
+		$collection=$db->usuarios;
+		$obj=array("usuario" => $user->getusername(),"contrasenia" =>$user->getpassword(),"nombre"=>$user->getfname(),"apellido"=>$user->getlname(),"telefono"=>$user->getphone(),"celular"=>$user->getcellphone(),"correo"=>$user->getemail());
+		try {
+			$collection->update(array('_id'=new MongoId($persona->getid())),array('$set'=>$obj));
+			return true;
+		} catch (Exception $e) {
+			return false;
 		}
 	}
 }
